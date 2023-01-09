87f46adc6078a0ddcd3eca5ffdc53061b0eb2afd
Tyler Gardner <tyler.gardner@maersk.com>
2023-01-09 13:53:02 -0700

    Data binding complete

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,11 +1,14 @@
-﻿namespace ToDoList;
+﻿using ToDoList.ViewModel;
+
+namespace ToDoList;
 
 public partial class MainPage : ContentPage
 {
 
-	public MainPage()
+	public MainPage(MainViewModel vm)
 	{
 		InitializeComponent();
+		BindingContext = vm;
 	}
 
 
